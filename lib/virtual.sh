561abfe11fe15b021815a4f5075507405bd4a75c
kurone-kito <krone@kit.black>
2026-02-01 00:14:25 +0900

    test: added the empty setup for the virtual environment

---
+++ lib/virtual.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# -*- mode: sh -*-
+# vim: set ft=sh :
+
+set -eu
+cd "$(cd "$(dirname "$0")"; pwd)/.."
+
+echo "Mode: virtual environment (Multipass via Terraform) -- not yet implemented"
