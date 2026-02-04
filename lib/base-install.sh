42d88b2236242863ae9c30b7c16baf4843b2c6b5
kurone-kito <krone@kit.black>
2026-02-04 22:31:47 +0900

    feat: update base-install.sh to install packages from cloud-init.yaml

--- lib/base-install.sh
+++ lib/base-install.sh
@@ -9,3 +9,7 @@ sudo apt-get update
 sudo apt-get upgrade -y
 
 sudo apt-get install -y --no-install-recommends yq
+
+# NOTE: The yq from apt is kislyuk/yq.
+mapfile -t packages < <(yq -r '.packages | sort | .[]' cloud-init.yml)
+sudo apt-get install -y --no-install-recommends "${packages[@]}"
