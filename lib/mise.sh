c11725be97db5667219dde1f6adea336f2f15903
kurone-kito <krone@kit.black>
2026-03-15 03:12:47 +0900

    feat: add the node.js installation

--- lib/mise.sh
+++ lib/mise.sh
@@ -11,3 +11,10 @@ mise install dotnet@8
 mise install dotnet@10
 mise install dotnet@latest
 mise use -g dotnet@latest
+
+mise install node@20
+mise install node@22
+mise install node@24
+mise install node@25
+mise install node@latest
+mise use -g node@latest
