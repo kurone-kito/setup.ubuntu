1b26819d50469d52fa5e298c2ac2d3f4ed70f94f
kurone-kito <krone@kit.black>
2026-03-15 03:12:47 +0900

    feat: added the bitwarden cli installation

--- lib/mise.sh
+++ lib/mise.sh
@@ -8,3 +8,9 @@ cd "$(cd "$(dirname "$0")"; pwd)/.."
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 SHELL="$(getent passwd | grep "$(whoami)" | cut -d: -f7 | xargs -I{} basename {})"
 eval "$(mise activate bash --shims)"
+
+mise install node@latest
+mise use -g node@latest
+
+mise install npm:@bitwarden/cli
+mise use -g npm:@bitwarden/cli
