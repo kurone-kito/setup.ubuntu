af8740bda73c5e0a9e22dd1ca94121444aeb4dd8
kurone-kito <krone@kit.black>
2026-03-15 03:12:32 +0900

    feat: add the dotnet installation

--- lib/mise.sh
+++ lib/mise.sh
@@ -6,3 +6,8 @@ set -eu
 cd "$(cd "$(dirname "$0")"; pwd)/.."
 
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+
+mise install dotnet@8
+mise install dotnet@10
+mise install dotnet@latest
+mise use -g dotnet@latest
