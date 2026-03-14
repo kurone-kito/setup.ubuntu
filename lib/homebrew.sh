aaebec773117b1d9f0bf619cf05b8549d2dff6f8
kurone-kito <krone@kit.black>
2026-03-15 02:48:11 +0900

    fix: ensure homebrew environment is set up correctly

--- lib/homebrew.sh
+++ lib/homebrew.sh
@@ -8,10 +8,8 @@ cd "$(cd "$(dirname "$0")"; pwd)/.."
 if [ ! -d "/home/linuxbrew/.linuxbrew" ]
 then
   curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh | bash -
-  echo >> "${HOME}/.bashrc"
-  echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> "${HOME}/.bashrc"
-  eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 fi
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 
 brew update
 brew bundle --file=lib/Brewfile
